81132d11474b6494f5457db451d479da5e9e7983
Maurizio Pietrantuono <MPI20@ukc02g143vmd6t.home>
2024-02-19 12:31:28 +0000

    OK, with a little delay

--- app/src/main/java/com/pietrantuono/columnsync/MainActivity.kt
+++ app/src/main/java/com/pietrantuono/columnsync/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.pietrantuono.columnsync
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,6 +19,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -30,33 +31,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ColumnSyncTheme {
-                SyncColumn()
+                SyncedColumn()
             }
         }
     }
 }
 
 @Composable
-private fun SyncColumn() {
+private fun SyncedColumn() {
     Row(
-        Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
     ) {
-        val scroll = rememberLazyListState()
-        val offset = rememberCurrentOffset(scroll)
+        val stateRowOne = rememberLazyListState()
+        val stateRowTwo = rememberLazyListState()
+
+        val key1 by remember {
+            derivedStateOf {
+                stateRowOne.firstVisibleItemScrollOffset
+            }
+        }
+        LaunchedEffect(key1) {
+            stateRowTwo.scrollToItem(
+                stateRowOne.firstVisibleItemIndex,
+                stateRowOne.firstVisibleItemScrollOffset,
+            )
+        }
+
         LazyColumn(
-            modifier = Modifier.weight(1f), state = scroll
+            modifier = Modifier.weight(1f),
+            state = stateRowOne,
         ) {
             items(100) {
                 Text("Item $it")
             }
         }
         Spacer(modifier = Modifier.width(48.dp))
-        val synced = rememberLazyListState()
-        LaunchedEffect(offset.value) {
-            synced.scrollBy(offset.value.toFloat())
+
+        val key2 by remember {
+            derivedStateOf {
+                stateRowTwo.firstVisibleItemScrollOffset
+            }
+        }
+
+        LaunchedEffect(key2) {
+            stateRowOne.scrollToItem(
+                stateRowTwo.firstVisibleItemIndex,
+                stateRowTwo.firstVisibleItemScrollOffset,
+            )
         }
         LazyColumn(
-            modifier = Modifier.weight(1f), state = synced
+            modifier = Modifier.weight(1f),
+            state = stateRowTwo,
         ) {
             items(100) {
                 Text("Item $it")
@@ -65,58 +91,3 @@ private fun SyncColumn() {
     }
 }
 
-
-@Composable
-fun rememberCurrentOffset(state: LazyListState): State<Int> {
-    val position = remember { derivedStateOf { state.firstVisibleItemIndex } }
-    val itemOffset = remember { derivedStateOf { state.firstVisibleItemScrollOffset } }
-    val lastPosition = rememberPrevious(position.value)
-    val lastItemOffset = rememberPrevious(itemOffset.value)
-    val currentOffset = remember { mutableStateOf(0) }
-
-    LaunchedEffect(position.value, itemOffset.value) {
-        if (lastPosition == null || position.value == 0) {
-            currentOffset.value = itemOffset.value
-        } else if (lastPosition == position.value) {
-            currentOffset.value += (itemOffset.value - (lastItemOffset ?: 0))
-        } else if (lastPosition > position.value) {
-            currentOffset.value -= (lastItemOffset ?: 0)
-        } else { // lastPosition.value < position.value
-            currentOffset.value += itemOffset.value
-        }
-    }
-
-    return currentOffset
-}
-
-@Composable
-fun <T> rememberPrevious(
-    current: T,
-    shouldUpdate: (prev: T?, curr: T) -> Boolean = { a: T?, b: T -> a != b },
-): T? {
-    val ref = rememberRef<T>()
-
-    // launched after render, so the current render will have the old value anyway
-    SideEffect {
-        if (shouldUpdate(ref.value, current)) {
-            ref.value = current
-        }
-    }
-
-    return ref.value
-}
-
-@Composable
-fun <T> rememberRef(): MutableState<T?> {
-    // for some reason it always recreated the value with vararg keys,
-    // leaving out the keys as a parameter for remember for now
-    return remember() {
-        object : MutableState<T?> {
-            override var value: T? = null
-
-            override fun component1(): T? = value
-
-            override fun component2(): (T?) -> Unit = { value = it }
-        }
-    }
-}
